10df455b6eb5fa196f7ca066ee1b88b3af259e36
Piotr Tyrlicz <tyrlicz@wp.pl>
2017-10-11 19:51:32 +0200

    "Pierwsza część zadania 1"

---
+++ src/Person.java
@@ -0,0 +1,46 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String pesel;
+
+    public void setFirstName(String firstName){
+        this.firstName=firstName;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+    public String getPesel() {
+        return pesel;
+    }
+
+    public Person(String firstName, String lastName, int age, String pesel){
+        if(firstName==null || lastName==null || firstName.length()<3 || lastName.length()<3)
+            throw new NameUndefinedException();
+        if (age<1)
+            throw new IncorrectAgeException();
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.age=age;
+        this.pesel=pesel;
+    }
+
+}
---
+++ src/Test.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Start programu");
+        Scanner scan1=new Scanner(System.in);
+        System.out.println("Podaj imie");
+        String imie=scan1.nextLine();
+        System.out.println("Podaj nazwisko");
+        String nazwisko=scan1.nextLine();
+        System.out.println("Podaj wiek");
+        int wiek=scan1.nextInt();
+        scan1.nextLine();
+        System.out.println("Podaj pesel");
+        String pesel=scan1.nextLine();
+        try {
+            Person person1 = new Person(imie, nazwisko, wiek, pesel);
+            System.out.println("Osoba "+ person1.getFirstName() + " " + person1.getLastName()+ " " + person1.getAge()+ " " + person1.getPesel());
+        }catch (NameUndefinedException e){
+            System.out.println("Coś nie tak z imieniem/nazwiskiem");
+        }catch (IncorrectAgeException e){
+            System.out.println("Cos nie tak z wiekiem");
+        }
+
+
+        System.out.println("Koniec programu");
+    }
+}
